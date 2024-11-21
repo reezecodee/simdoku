2fcb1fc353f99c8e84fe538986dbc8139e09d38e
neibreen <azfaalharits25@gmail.com>
2024-11-21 12:09:09 +0700

    feat: create letter page

--- resources/views/livewire/letter/assignment-modify.blade.php
+++ resources/views/livewire/letter/assignment-modify.blade.php
@@ -3,11 +3,141 @@
         <div class="card">
             <div class="card-body">
                 <div class="d-flex justify-content-end">
-                    <button wire:click="downloadPDF" class="btn btn-success mr-2">Download PDF</button>
+                    <button wire:click="downloadPDF" class="btn btn-success mr-2"><i class="fas fa-file-pdf"></i> Download
+                        PDF</button>
+                    <button wire:click="downloadWord" class="btn btn-primary mr-2"><i class="fas fa-file-word"></i>
+                        Download Word</button>
                     <a href="{{ route('letter.assignment') }}" wire:navigate.hover>
-                        <button class="btn btn-danger">Kembali</button>
+                        <button class="btn btn-danger"><i class="fas fa-chevron-left"></i> Kembali</button>
                     </a>
                 </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="" class="form-label">Kepala Divisi MER Universitas BSI</label>
+                            <input placeholder="Masukkan nama kepala divisi MER" type="text" class="form-control"
+                                wire:model.live.debounce.500ms="">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="" class="form-label">Perihal surat</label>
+                            <input placeholder="Masukkan perihal surat" type="text" class="form-control"
+                                wire:model.live.debounce.500ms="">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="" class="form-label">Kegiatan tugas</label>
+                            <input placeholder="Masukkan kegiatan tugas" type="text" class="form-control"
+                                wire:model.live.debounce.500ms="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <b>Staff yang akan bertugas</b>
+                </div>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>NIP</th>
+                            <th>Nama</th>
+                            <th>Sekolah</th>
+                            <th>Tanggal pelaksanaan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>
+                                <input type="number" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="NIP staff">
+                            </td>
+                            <td>
+                                <input type="text" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="Nama staff">
+                            </td>
+                            <td rowspan="3">
+                                <textarea cols="30" rows="10" wire:model.live.debounce.500ms="" class="form-control"
+                                    placeholder="Nama sekolah"></textarea>
+                            </td>
+                            <td rowspan="3">
+                                <textarea cols="30" rows="10" wire:model.live.debounce.500ms="" class="form-control"
+                                    placeholder="Hari, tanggal, dan jam"></textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>
+                                <input type="number" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="NIP staff">
+                            </td>
+                            <td>
+                                <input type="text" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="Nama staff">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <b>Volunteer yang akan bertugas</b>
+                </div>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>NIM</th>
+                            <th>Nama</th>
+                            <th>Sekolah</th>
+                            <th>Tanggal pelaksanaan</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>
+                                <input type="number" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="NIP staff">
+                            </td>
+                            <td>
+                                <input type="text" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="Nama staff">
+                            </td>
+                            <td rowspan="3">
+                                <textarea cols="30" rows="10" wire:model.live.debounce.500ms="" class="form-control"
+                                    placeholder="Nama sekolah"></textarea>
+                            </td>
+                            <td rowspan="3">
+                                <textarea cols="30" rows="10" wire:model.live.debounce.500ms="" class="form-control"
+                                    placeholder="Hari, tanggal, dan jam"></textarea>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>
+                                <input type="number" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="NIP staff">
+                            </td>
+                            <td>
+                                <input type="text" wire:model.live.debounce.500ms class="form-control"
+                                    placeholder="Nama staff">
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
--- resources/views/livewire/letter/assignment.blade.php
+++ resources/views/livewire/letter/assignment.blade.php
@@ -2,10 +2,81 @@
     <div class="section-body">
         <div class="card">
             <div class="card-body">
-                <div class="d-flex justify-content-end">
+                <div class="d-flex justify-content-end mb-3">
                     <a wire:click="createLetterAssigment">
                         <button class="btn btn-primary">Buat Surat Tugas</button>
                     </a>
+                </div>
+                <div class="table-responsive table-invoice">
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Judul surat</th>
+                            <th>Surat tugas kegiatan</th>
+                            <th>Dibuat pada</th>
+                            <th>Terakhir diperbarui</th>
+                            <th>Action</th>
+                        </tr>
+                        <tr>
+                            <td>INV-87239</td>
+                            <td>INV-87239</td>
+                            <td>INV-87239</td>
+                            <td>INV-87239</td>
+                            <td>
+                                <a href="#" class="btn btn-success"><i class="fas fa-file-pdf"></i> PDF</a>
+                                <a href="#" class="btn btn-primary"><i class="fas fa-file-word"></i> Word</a>
+                                <a href="#" class="btn btn-warning"><i class="fas fa-edit"></i> Edit</a>
+                                <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i> Hapus</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>INV-48574</td>
+                            <td>INV-48574</td>
+                            <td>INV-48574</td>
+                            <td>INV-48574</td>
+                            <td>
+                                <a href="#" class="btn btn-success"><i class="fas fa-file-pdf"></i> PDF</a>
+                                <a href="#" class="btn btn-primary"><i class="fas fa-file-word"></i> Word</a>
+                                <a href="#" class="btn btn-warning"><i class="fas fa-edit"></i> Edit</a>
+                                <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i> Hapus</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>INV-76824</td>
+                            <td>INV-76824</td>
+                            <td>INV-76824</td>
+                            <td>INV-76824</td>
+                            <td>
+                                <a href="#" class="btn btn-success"><i class="fas fa-file-pdf"></i> PDF</a>
+                                <a href="#" class="btn btn-primary"><i class="fas fa-file-word"></i> Word</a>
+                                <a href="#" class="btn btn-warning"><i class="fas fa-edit"></i> Edit</a>
+                                <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i> Hapus</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>INV-84990</td>
+                            <td>INV-84990</td>
+                            <td>INV-84990</td>
+                            <td>INV-84990</td>
+                            <td>
+                                <a href="#" class="btn btn-success"><i class="fas fa-file-pdf"></i> PDF</a>
+                                <a href="#" class="btn btn-primary"><i class="fas fa-file-word"></i> Word</a>
+                                <a href="#" class="btn btn-warning"><i class="fas fa-edit"></i> Edit</a>
+                                <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i> Hapus</a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>INV-87320</td>
+                            <td>INV-87320</td>
+                            <td>INV-87320</td>
+                            <td>INV-87320</td>
+                            <td>
+                                <a href="#" class="btn btn-success"><i class="fas fa-file-pdf"></i> PDF</a>
+                                <a href="#" class="btn btn-primary"><i class="fas fa-file-word"></i> Word</a>
+                                <a href="#" class="btn btn-warning"><i class="fas fa-edit"></i> Edit</a>
+                                <a href="#" class="btn btn-danger"><i class="fas fa-trash"></i> Hapus</a>
+                            </td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
