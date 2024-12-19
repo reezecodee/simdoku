4ec7cd386a8bea8fe6f6c59530113be44979c096
neibreen <azfaalharits25@gmail.com>
2024-12-19 07:46:07 +0700

    first commit

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,11 +10,6 @@
     <link rel="stylesheet" href="/assets/modules/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/modules/fontawesome/css/all.min.css">
 
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="/assets/modules/jqvmap/dist/jqvmap.min.css">
-    <link rel="stylesheet" href="/assets/modules/summernote/summernote-bs4.css">
-    <link rel="stylesheet" href="/assets/modules/owlcarousel2/dist/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="/assets/modules/owlcarousel2/dist/assets/owl.theme.default.min.css">
 
     <!-- Template CSS -->
     <link rel="stylesheet" href="/assets/css/style.css">
@@ -54,22 +49,10 @@
     <script src="/assets/modules/tooltip.js"></script>
     <script src="/assets/modules/bootstrap/js/bootstrap.min.js"></script>
     <script src="/assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-    <script src="/assets/modules/moment.min.js"></script>
     <script src="/assets/js/stisla.js"></script>
-
-    <!-- JS Libraies -->
-    <script src="/assets/modules/jquery.sparkline.min.js"></script>
-    <script src="/assets/modules/chart.min.js"></script>
-    <script src="/assets/modules/owlcarousel2/dist/owl.carousel.min.js"></script>
-    <script src="/assets/modules/summernote/summernote-bs4.js"></script>
-    <script src="/assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
-
-    <!-- Page Specific JS File -->
-    <script src="/assets/js/page/index.js"></script>
 
     <!-- Template JS File -->
     <script src="/assets/js/scripts.js"></script>
-    <script src="/assets/js/custom.js"></script>
     @livewireScripts
 </body>
 
--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg main-navbar">
-    <a href="index.html" class="navbar-brand sidebar-gone-hide">Ape Tu Woi</a>
+    <a href="index.html" class="navbar-brand sidebar-gone-hide">Archivin</a>
     <a href="#" class="nav-link sidebar-gone-show" data-toggle="sidebar"><i class="fas fa-bars"></i></a>
     <div class="nav-collapse">
       <a class="sidebar-gone-show nav-collapse-toggle nav-link" href="#">
