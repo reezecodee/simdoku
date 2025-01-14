df5d51d36951395376f8160b3ebb6daca4a0be99
Reeze <azfaalharits25@gmail.com>
2025-01-14 21:14:39 +0700

    style: bordered table staff

--- resources/views/livewire/components/staff-table.blade.php
+++ resources/views/livewire/components/staff-table.blade.php
@@ -1,4 +1,12 @@
 <div>
+    <style>
+        .custom-table th,
+        .custom-table td {
+            border: 1px solid #ddd;
+            padding: 8px;
+        }
+    </style>
+
     <div class="d-flex justify-content-end">
         <button class="btn btn-primary mb-3 mr-2" wire:click="addExecutionStaff('{{ $letterId }}')">Tambah
             Pelaksanaan</button>
@@ -10,7 +18,8 @@
         </button>
         @endforeach
         @else
-        <select class="form-control w-25" wire:model.defer="selectedExecutionId" wire:change="addStaffFromDropdown($event.target.value)">
+        <select class="form-control w-25" wire:model.defer="selectedExecutionId"
+            wire:change="addStaffFromDropdown($event.target.value)">
             <option selected>Tambah Staff</option>
             @foreach ($executionStaffs as $execution)
             <option value="{{ $execution->id }}">Tambah staff pelaksanaan {{ $loop->iteration }}</option>
@@ -18,14 +27,14 @@
         </select>
         @endif
     </div>
-   
-    <table class="table table-striped">
+
+    <table class="table table-striped custom-table">
         <thead>
             <tr>
                 <th>No</th>
                 <th>NIP</th>
                 <th>Nama</th>
-                <th>Aksi</th>
+                <th></th>
                 <th>Sekolah</th>
                 <th>Tanggal Pelaksanaan</th>
                 <th>Aksi</th>
@@ -34,24 +43,24 @@
         <tbody>
             @foreach ($executionStaffs as $execution)
             @php
-                $relatedStaffs = $execution->staff;
+            $relatedStaffs = $execution->staff;
             @endphp
             <tr>
                 <td rowspan="{{ max($relatedStaffs->count(), 1) }}">{{ $loop->iteration }}</td>
                 @if($relatedStaffs->count() == 0)
-                    <td colspan="3">Belum ada anggota</td>
+                <td colspan="3">Belum ada anggota</td>
                 @else
-                    <td>
-                        <input type="text" class="form-control" value="{{ $relatedStaffs->first()->nip }}">
-                    </td>
-                    <td>
-                        <input type="text" class="form-control" value="{{ $relatedStaffs->first()->nama }}">
-                    </td>
-                    <td>
-                        <button class="btn btn-danger" wire:click="deleteStaff('{{ $relatedStaffs->first()->id }}')">
-                            <i class="fas fa-trash"></i>
-                        </button>
-                    </td>
+                <td>
+                    <input type="text" class="form-control" value="{{ $relatedStaffs->first()->nip }}">
+                </td>
+                <td>
+                    <input type="text" class="form-control" value="{{ $relatedStaffs->first()->nama }}">
+                </td>
+                <td>
+                    <button class="btn btn-danger" wire:click="deleteStaff('{{ $relatedStaffs->first()->id }}')">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </td>
                 @endif
                 <td rowspan="{{ max($relatedStaffs->count(), 1) }}">
                     <input type="text" class="form-control" value="{{ $execution->nama_sekolah }}">
@@ -65,27 +74,27 @@
                     </button>
                 </td>
             </tr>
-    
+
             @if($relatedStaffs->count() > 1)
-                @foreach ($relatedStaffs->skip(1) as $staff)
-                <tr>
-                    <td>
-                        <input type="text" class="form-control" value="{{ $staff->nip }}">
-                    </td>
-                    <td>
-                        <input type="text" class="form-control" value="{{ $staff->nama }}">
-                    </td>
-                    <td>
-                        <button class="btn btn-danger" wire:click="deleteStaff('{{ $staff->id }}')">
-                            <i class="fas fa-trash"></i>
-                        </button>
-                    </td>
-                </tr>
-                @endforeach
+            @foreach ($relatedStaffs->skip(1) as $staff)
+            <tr>
+                <td>
+                    <input type="text" class="form-control" value="{{ $staff->nip }}">
+                </td>
+                <td>
+                    <input type="text" class="form-control" value="{{ $staff->nama }}">
+                </td>
+                <td>
+                    <button class="btn btn-danger" wire:click="deleteStaff('{{ $staff->id }}')">
+                        <i class="fas fa-trash"></i>
+                    </button>
+                </td>
+            </tr>
+            @endforeach
             @endif
             @endforeach
         </tbody>
     </table>
-    
+
 </div>
 </div>
