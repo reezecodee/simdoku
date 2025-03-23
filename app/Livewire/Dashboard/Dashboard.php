c8e8a2f72e86a4e3eb1acefecbf649a5f05f377b
Reeze <azfaalharits25@gmail.com>
2025-03-23 17:18:05 +0700

    fix: dashboard

--- app/Livewire/Dashboard/Dashboard.php
+++ app/Livewire/Dashboard/Dashboard.php
@@ -2,6 +2,11 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\Formulir;
+use App\Models\LetterAssignment;
+use App\Models\Proposal;
+use App\Models\Report;
+use App\Models\Scholarship;
 use Livewire\Attributes\Title;
 use Livewire\Component;
 
@@ -10,6 +15,16 @@ class Dashboard extends Component
     #[Title('Dashboard')]
 
     public $title = 'Dashboard';
+    public $scholarship, $letter, $proposal, $report, $formulir;
+
+    public function mount()
+    {
+        $this->scholarship = Scholarship::count();
+        $this->letter = LetterAssignment::count();
+        $this->proposal = Proposal::count();
+        $this->report = Report::count();
+        $this->formulir = Formulir::count();
+    }
 
     public function render()
     {
--- resources/views/livewire/dashboard/dashboard.blade.php
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -1,60 +1,88 @@
 <div>
-    <div class="section-header">
-        <h1>{{ $title }}</h1>
-        <div class="section-header-breadcrumb">
-            <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-            <div class="breadcrumb-item"><a href="#">Layout</a></div>
-            <div class="breadcrumb-item">Top Navigation</div>
-        </div>
+  <div class="section-header">
+    <h1>{{ $title }}</h1>
+    <div class="section-header-breadcrumb">
+      <div class="breadcrumb-item">Dashboard Monitoring Total</div>
     </div>
+  </div>
 
-    <div class="section-body">
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-12">
-              <div class="card card-statistic-2">
-                <div class="card-icon shadow-primary bg-primary">
-                  <i class="fas fa-archive"></i>
-                </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Total Orders</h4>
-                  </div>
-                  <div class="card-body">
-                    59
-                  </div>
-                </div>
-              </div>
+  <div class="section-body">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+          <div class="card-icon shadow-primary bg-primary">
+            <i class="fas fa-graduation-cap"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Beasiswa</h4>
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-              <div class="card card-statistic-2">
-                <div class="card-icon shadow-primary bg-primary">
-                  <i class="fas fa-archive"></i>
-                </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Total Orders</h4>
-                  </div>
-                  <div class="card-body">
-                    59
-                  </div>
-                </div>
-              </div>
+            <div class="card-body">
+              {{ $scholarship }}
             </div>
-            <div class="col-lg-4 col-md-4 col-sm-12">
-              <div class="card card-statistic-2">
-                <div class="card-icon shadow-primary bg-primary">
-                  <i class="fas fa-archive"></i>
-                </div>
-                <div class="card-wrap">
-                  <div class="card-header">
-                    <h4>Total Orders</h4>
-                  </div>
-                  <div class="card-body">
-                    59
-                  </div>
-                </div>
-              </div>
-            </div>
+          </div>
         </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+          <div class="card-icon shadow-primary bg-primary">
+            <i class="fas fa-mail-bulk"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Surat Tugas</h4>
+            </div>
+            <div class="card-body">
+              {{ $letter }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+          <div class="card-icon shadow-primary bg-primary">
+            <i class="fas fa-scroll"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Proposal</h4>
+            </div>
+            <div class="card-body">
+              {{ $proposal }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+          <div class="card-icon shadow-primary bg-primary">
+            <i class="fas fa-paste"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Laporan</h4>
+            </div>
+            <div class="card-body">
+              {{ $report }}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="card card-statistic-2">
+          <div class="card-icon shadow-primary bg-primary">
+            <i class="fas fa-table"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Formulir Pengajuan</h4>
+            </div>
+            <div class="card-body">
+              {{ $formulir }}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
