f7e22dad664dad1fa16f94e2c2c42bfacff8ed65
Reeze <azfaalharits25@gmail.com>
2025-04-12 15:48:48 +0700

    fix: datatable formulir

--- app/Http/Controllers/Datatable/FundApplicationDatatableController.php
+++ app/Http/Controllers/Datatable/FundApplicationDatatableController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Datatable;
 
 use App\Http\Controllers\Controller;
 use App\Models\Formulir;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
 
@@ -14,31 +15,31 @@ class FundApplicationDatatableController extends Controller
         $funds = Formulir::orderBy('created_at', 'desc')->get();
 
         return DataTables::of($funds)
-        ->addIndexColumn()
-        ->addColumn('judul', function($row){
-            return $row->judul ?? 'Judul tidak diketahui';
-        })
-        ->addColumn('tgl_pengajuan', function($row){
-            return $row->tgl_pengajuan ?? 'Belum diatur';
-        })
-        ->addColumn('tgl_diperlukan', function($row){
-            return $row->tgl_diperlukan ?? 'Belum diatur';
-        })
-        ->addColumn('action', function($row){
-            return '
+            ->addIndexColumn()
+            ->addColumn('judul', function ($row) {
+                return $row->judul ?? 'Judul tidak diketahui';
+            })
+            ->addColumn('tgl_pengajuan', function ($row) {
+                return $row->tgl_pengajuan ? Carbon::parse($row->tgl_pengajuan)->translatedFormat('l, d F Y') : 'Belum diatur';
+            })
+            ->addColumn('tgl_diperlukan', function ($row) {
+                return $row->tgl_diperlukan ? Carbon::parse($row->tgl_diperlukan)->translatedFormat('l, d F Y') : 'Belum diatur';
+            })
+            ->addColumn('action', function ($row) {
+                return '
             <div class="d-flex">
-            <a wire:navigate class="mr-2 d-inline-block" href="'. route('form.modify', $row->id) .'" style="text-decoration: none;">
+            <a wire:navigate class="mr-2 d-inline-block" href="' . route('form.modify', $row->id) . '" style="text-decoration: none;">
             <button class="btn btn-primary btn-sm">Edit</button>
             </a>
-            <form action="'. route('form.delete', $row->id) .'" method="POST" id="'. $row->id .'">
-            '. csrf_field() .'
-            '. method_field('DELETE') .'
+            <form action="' . route('form.delete', $row->id) . '" method="POST" id="' . $row->id . '">
+            ' . csrf_field() . '
+            ' . method_field('DELETE') . '
             <button class="btn btn-danger btn-sm" onclick="submitForm(\'' . $row->id . '\')" type="button">Hapus</button>
             </form>
             </div>
             ';
-        })
-        ->rawColumns(['action']) 
-        ->make(true);
+            })
+            ->rawColumns(['action'])
+            ->make(true);
     }
 }
